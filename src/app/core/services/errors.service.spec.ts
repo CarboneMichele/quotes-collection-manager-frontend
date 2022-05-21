ab17709e332bb587548433b037b0df18e33c8f1d
michelecarbone <carbone.michele.work@gmail.com>
2022-05-21 12:16:06 +0200

    feat: added err service

--- src/app/core/services/errors.service.spec.ts
+++ src/app/core/services/errors.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { ErrorsService } from './errors.service';
 
 describe('ErrorsService', () => {
-  let service: ErrorsService;
+    let service: ErrorsService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ErrorsService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(ErrorsService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });

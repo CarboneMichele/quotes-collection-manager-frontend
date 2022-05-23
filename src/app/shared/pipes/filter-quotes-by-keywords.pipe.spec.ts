a641b7c1f7c5ecabebbe09622fb86eaa796415ab
michelecarbone <carbone.michele.work@gmail.com>
2022-05-23 15:49:38 +0200

    test: added cypress

--- src/app/shared/pipes/filter-quotes-by-keywords.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterQuotesByKeywordsPipe } from './filter-quotes-by-keywords.pipe';
-
-describe('FilterQuotesByKeywordsPipe', () => {
-    it('create an instance', () => {
-        const pipe = new FilterQuotesByKeywordsPipe();
-        expect(pipe).toBeTruthy();
-    });
-});
